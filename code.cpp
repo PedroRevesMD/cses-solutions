a968679472cc68edc43fead7ed644809f4966d03
PedroRevesMD <pedroreves11@gmail.com>
2026-01-02 21:29:49 -0300

    First CSES challenge

---
+++ code.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long int;
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  ll n;
+  cin >> n;
+
+  while (n != 1) {
+    cout << n << " ";
+
+    if (n & 1LL) {
+      n = n * 3 + 1;
+    } else {
+      n /= 2;
+    }
+  }
+
+  cout << 1;
+
+  return 0;
+}
